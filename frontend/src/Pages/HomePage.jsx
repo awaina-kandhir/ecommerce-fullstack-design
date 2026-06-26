9c39ce168f23b1af161af07ef102678ec6565d7f
awaina-kandhir <awainakandhir@gmail.com>
2026-06-27 02:25:51 +0500

    Fix Footer filename case

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 
 function HomePage() {
   const [products, setProducts] = useState([]);
